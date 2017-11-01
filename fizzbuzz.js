1c7c69320fbed37f8f17952087241f117871e825
Achia Rolence <akarolence12@gmail.com>
2017-11-01 14:43:46 +0100

    updated fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,23 +4,19 @@ number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
 When you have that working, modify your program to print "FizzBuzz"
 for numbers that are divisible by both 3 and 5.
  */
-var number = 1;
+var number = 1; 
 while (number <= 100) {
-    if(number %3 == 0 && number % 5 == 0)
-    {
-        console.log("FIZZBUZZ");
-    }
-    else if(number % 3 == 0)
-    {
-        console.log("FIZZ");
-    }
-    else if (number % 5 == 0)
-    {
-        console.log("BUZZ");
-    }
-    else
-    {
-        console.log(number);
-    }
-    number +=1;
+if (number % 3 == 0 && number % 5 == 0) {
+console.log("FIZZBUZZ"); 
+}
+else if (number % 3 == 0) {
+console.log("FIZZ"); 
+}
+else if (number % 5 == 0) {
+console.log("BUZZ"); 
+}
+else {
+console.log(number); 
+}
+number += 1; 
 }
